8d49ba73037cbe36e2668dca007c734bcc818f73
LComora <comolarulas@gmail.com>
2022-05-19 20:31:54 -0300

    Inclusão das Demos de C#

---
+++ Demo3/Objetos.cs
@@ -0,0 +1,16 @@
+public ref struct Pessoa
+{
+    public int Idade { get; set; }
+    public string Nome { get; set }
+
+    public Endereco EnderecoPessoa { get; set; }
+}
+
+public ref struct Endereco
+{
+    public int Numero { get; set; }
+    public string Logradouro { get; set; }
+    public string CEP { get; set; }
+    public string Cidade { get; set; }
+
+}
---
+++ Demo3/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Ricardo";
+p1.Idade = 30;
+
+p1.EnderecoPessoa = new Endereco();
+{
+    Logradouro = "Rua teste",
+    Numero = 300,
+    CEP = "00000",
+    Cidade = "São Paulo",
+};
+
+WriteLine("Fim");
